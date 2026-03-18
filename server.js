d667d7f039687923fa443050473b66bc73510e20
nienna <53435346+nienna6@users.noreply.github.com>
2026-03-18 04:33:17 -0400

    Serie: Aida

--- server.js
+++ server.js
@@ -51,6 +51,7 @@ const categories = [
 
 // 3. Series
 const series = [
+  {"series_id":1773822749161,"name":"Aida","cover":"https://es.web.img3.acsta.net/pictures/14/02/07/11/08/378771.jpg","plot":"Serie sitcom Española","category_id":"","num":1},
   {
     series_id: 1,
     name: "Pluribus",
@@ -67,6 +68,242 @@ const seriesCategories = [
 ];
 
 const seriesEpisodes = {
+  1773822749161: {
+  "seasons": [
+    {
+      "season_number": 1,
+      "name": "Temporada 1",
+      "episode_count": 25
+    },
+    {
+      "season_number": 2,
+      "name": "Temporada 2",
+      "episode_count": null
+    },
+    {
+      "season_number": 3,
+      "name": "Temporada 3",
+      "episode_count": null
+    },
+    {
+      "season_number": 4,
+      "name": "Temporada 4",
+      "episode_count": null
+    },
+    {
+      "season_number": 5,
+      "name": "Temporada 5",
+      "episode_count": null
+    },
+    {
+      "season_number": 6,
+      "name": "Temporada 6",
+      "episode_count": null
+    },
+    {
+      "season_number": 7,
+      "name": "Temporada 7",
+      "episode_count": null
+    },
+    {
+      "season_number": 8,
+      "name": "Temporada 8",
+      "episode_count": null
+    },
+    {
+      "season_number": 9,
+      "name": "Temporada 9",
+      "episode_count": null
+    }
+  ],
+  "episodes": {
+    "1": [
+      {
+        "id": "177382274916111",
+        "episode_num": 1,
+        "title": "Episodio 1",
+        "container_extension": "mp4",
+        "direct_source": "https://cdnvideo.davidmonrroy7.workers.dev/video/https%3A%2F%2Farchive.org%2Fdownload%2Fdercro-2040%2FDERCRO2040.mkv"
+      },
+      {
+        "id": "177382274916112",
+        "episode_num": 2,
+        "title": "Episodio 2",
+        "container_extension": "mp4",
+        "direct_source": null
+      },
+      {
+        "id": "177382274916113",
+        "episode_num": 3,
+        "title": "Episodio 3",
+        "container_extension": "mp4",
+        "direct_source": null
+      },
+      {
+        "id": "177382274916114",
+        "episode_num": 4,
+        "title": "Episodio 4",
+        "container_extension": "mp4",
+        "direct_source": ""
+      },
+      {
+        "id": "177382274916115",
+        "episode_num": 5,
+        "title": "Episodio 5",
+        "container_extension": "mp4",
+        "direct_source": ""
+      },
+      {
+        "id": "177382274916116",
+        "episode_num": 6,
+        "title": "Episodio 6",
+        "container_extension": "mp4",
+        "direct_source": null
+      },
+      {
+        "id": "177382274916117",
+        "episode_num": 7,
+        "title": "Episodio 7",
+        "container_extension": "mp4",
+        "direct_source": null
+      },
+      {
+        "id": "177382274916118",
+        "episode_num": 8,
+        "title": "Episodio 8",
+        "container_extension": "mp4",
+        "direct_source": null
+      },
+      {
+        "id": "177382274916119",
+        "episode_num": 9,
+        "title": "Episodio 9",
+        "container_extension": "mp4",
+        "direct_source": null
+      },
+      {
+        "id": "1773822749161110",
+        "episode_num": 10,
+        "title": "Episodio 10",
+        "container_extension": "mp4",
+        "direct_source": null
+      },
+      {
+        "id": "1773822749161111",
+        "episode_num": 11,
+        "title": "Episodio 11",
+        "container_extension": "mp4",
+        "direct_source": null
+      },
+      {
+        "id": "1773822749161112",
+        "episode_num": 12,
+        "title": "Episodio 12",
+        "container_extension": "mp4",
+        "direct_source": null
+      },
+      {
+        "id": "1773822749161113",
+        "episode_num": 13,
+        "title": "Episodio 13",
+        "container_extension": "mp4",
+        "direct_source": null
+      },
+      {
+        "id": "1773822749161114",
+        "episode_num": 14,
+        "title": "Episodio 14",
+        "container_extension": "mp4",
+        "direct_source": null
+      },
+      {
+        "id": "1773822749161115",
+        "episode_num": 15,
+        "title": "Episodio 15",
+        "container_extension": "mp4",
+        "direct_source": null
+      },
+      {
+        "id": "1773822749161116",
+        "episode_num": 16,
+        "title": "Episodio 16",
+        "container_extension": "mp4",
+        "direct_source": null
+      },
+      {
+        "id": "1773822749161117",
+        "episode_num": 17,
+        "title": "Episodio 17",
+        "container_extension": "mp4",
+        "direct_source": null
+      },
+      {
+        "id": "1773822749161118",
+        "episode_num": 18,
+        "title": "Episodio 18",
+        "container_extension": "mp4",
+        "direct_source": null
+      },
+      {
+        "id": "1773822749161119",
+        "episode_num": 19,
+        "title": "Episodio 19",
+        "container_extension": "mp4",
+        "direct_source": null
+      },
+      {
+        "id": "1773822749161120",
+        "episode_num": 20,
+        "title": "Episodio 20",
+        "container_extension": "mp4",
+        "direct_source": null
+      },
+      {
+        "id": "1773822749161121",
+        "episode_num": 21,
+        "title": "Episodio 21",
+        "container_extension": "mp4",
+        "direct_source": null
+      },
+      {
+        "id": "1773822749161122",
+        "episode_num": 22,
+        "title": "Episodio 22",
+        "container_extension": "mp4",
+        "direct_source": null
+      },
+      {
+        "id": "1773822749161123",
+        "episode_num": 23,
+        "title": "Episodio 23",
+        "container_extension": "mp4",
+        "direct_source": null
+      },
+      {
+        "id": "1773822749161124",
+        "episode_num": 24,
+        "title": "Episodio 24",
+        "container_extension": "mp4",
+        "direct_source": null
+      },
+      {
+        "id": "1773822749161125",
+        "episode_num": 25,
+        "title": "Episodio 25",
+        "container_extension": "mp4",
+        "direct_source": null
+      }
+    ],
+    "2": [],
+    "3": [],
+    "4": [],
+    "5": [],
+    "6": [],
+    "7": [],
+    "8": [],
+    "9": []
+  }
+},
   1: {
     seasons: [{ season_number: 1, name: "Temporada 1", episode_count: 1 }],
     episodes: {
